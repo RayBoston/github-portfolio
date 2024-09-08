47e4c69f1e6f925edfce932d7693894d241438c4
RayBoston <166935762+RayBoston@users.noreply.github.com>
2024-09-08 17:11:16 -0400

    add eventlistener on #contact-email-address to display email address when clicked

--- Contact Page/contact.js
+++ Contact Page/contact.js
@@ -1,16 +1,15 @@
-
+const email = document.getElementById("contact-email");
 const emailAddress = document.getElementById("contact-email-address");
 const emailDownCaret = document.getElementById("caret-down");
 const emailRightCaret = document.getElementById("caret-right");
 
+
 function showEmail() {
-    emailAddress.style.display = "initial";
-    emailDownCaret.style.display = "initial";
-    emailRightCaret.style.display = "none";
-}
-function hideEmail() {
-    emailAddress.style.display = "none";
-    emailDownCaret.style.display = "none";
-    emailRightCaret.style.display = "initial";
+        emailAddress.style.visibility = "visible";
+        emailDownCaret.style.display = "initial";
+        emailRightCaret.style.display = "none";
 }
 
+    email.onclick = showEmail;
+    emailRightCaret.onclick = showEmail;
+    email.removeEventListener(click, showEmail);
