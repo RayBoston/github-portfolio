a6410f3ded3f0f9bc5edadb44c90b6bb18ba2ad5
RayBoston <166935762+RayBoston@users.noreply.github.com>
2024-09-26 19:39:31 -0400

    assign the li elements and i elements of #My-Skills to variables

---
+++ index.js
@@ -0,0 +1,13 @@
+const htmlBox = document.getElementById("html-icon-box");
+const cssBox = document.getElementById("css-icon-box");
+const javascriptBox = document.getElementById("javascript-icon-box");
+const gitBox = document.getElementById("git-icon-box");
+const githubBox = document.getElementById("github-icon-box");
+const nodeBox = document.getElementById("node-icon-box");
+
+const htmlIcon = document.getElementsByClassName("fa-html5")[0];
+const cssIcon = document.getElementsByClassName("fa-css3")[0];
+const javascriptIcon = document.getElementsByClassName("fa-js")[0];
+const gitIcon = document.getElementsByClassName("fa-git")[0];
+const githubIcon = document.getElementsByClassName("fa-github")[0];
+const nodeIcon = document.getElementsByClassName("fa-node-js")[0];
