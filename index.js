d1e344349e68d1a8552bbf13353c43fb561fc11c
RayBoston <166935762+RayBoston@users.noreply.github.com>
2024-09-26 21:27:42 -0400

    create an event handler function and an event listener that changes the dimensons of the li elements on a click

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ const javascriptIcon = document.getElementsByClassName("fa-js")[0];
 const gitIcon = document.getElementsByClassName("fa-git")[0];
 const githubIcon = document.getElementsByClassName("fa-github")[0];
 const nodeIcon = document.getElementsByClassName("fa-node-js")[0];
+
+function defineLanguage(targetBox) {
+    if (targetBox.style.border !== "3px solid red") {
+        targetBox.style.border = "3px solid red";
+        targetBox.style.width = "250px";
+        targetBox.style.height = "500px";
+
+    } else {
+        targetBox.style.border = "0px";
+        targetBox.style.width = "initial";
+        targetBox.style.height = "initial";
+    }
+}
+
+htmlIcon.addEventListener("click", function () {defineLanguage(htmlBox)});
+cssIcon.addEventListener("click", function () {defineLanguage(cssBox)});
+javascriptIcon.addEventListener("click", function () {defineLanguage(javascriptBox)});
+gitIcon.addEventListener("click", function () {defineLanguage(gitBox)});
+githubIcon.addEventListener("click", function () {defineLanguage(githubBox)});
+nodeIcon.addEventListener("click", function () {defineLanguage(nodeBox)});
