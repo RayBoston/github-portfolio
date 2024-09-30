ec02b0f6e0e8fa37637d6a7520c6691950217f79
RayBoston <166935762+RayBoston@users.noreply.github.com>
2024-09-29 22:09:35 -0400

    change function name, make language description headers change color on a click, change visibility of language descriptions on click

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const htmlBox = document.getElementById("html-icon-box");
-const cssBox = document.getElementById("css-icon-box");
-const javascriptBox = document.getElementById("javascript-icon-box");
-const gitBox = document.getElementById("git-icon-box");
-const githubBox = document.getElementById("github-icon-box");
-const nodeBox = document.getElementById("node-icon-box");
 
 const htmlIcon = document.getElementsByClassName("fa-html5")[0];
 const cssIcon = document.getElementsByClassName("fa-css3")[0];
@@ -12,22 +6,34 @@ const gitIcon = document.getElementsByClassName("fa-git")[0];
 const githubIcon = document.getElementsByClassName("fa-github")[0];
 const nodeIcon = document.getElementsByClassName("fa-node-js")[0];
 
-function defineLanguage(targetBox) {
-    if (targetBox.style.border !== "3px solid red") {
-        targetBox.style.border = "3px solid red";
-        targetBox.style.width = "250px";
-        targetBox.style.height = "500px";
+const htmlHeading = document.getElementsByTagName("strong")[0];
+const cssHeading = document.getElementsByTagName("strong")[1]; 
+const javascriptHeading = document.getElementsByTagName("strong")[2]; 
+const gitHeading = document.getElementsByTagName("strong")[3]; 
+const githubHeading = document.getElementsByTagName("strong")[4]; 
+const nodeHeading = document.getElementsByTagName("strong")[5];  
+
+const htmlDescription = document.getElementsByTagName("span")[0];
+const cssDescription = document.getElementsByTagName("span")[1];
+const javascriptDescription = document.getElementsByTagName("span")[2];
+const gitDescription = document.getElementsByTagName("span")[3];
+const githubDescription = document.getElementsByTagName("span")[4];
+const nodeDescription = document.getElementsByTagName("span")[5];
+
+function showLanguageDescription(targetHeading, targetDescription) {
+    if (targetDescription.style.visibility === "hidden") {
+        targetDescription.style.visibility = "visible";
+        targetHeading.style.color = "red";
 
     } else {
-        targetBox.style.border = "0px";
-        targetBox.style.width = "initial";
-        targetBox.style.height = "initial";
+        targetDescription.style.visibility = "hidden";
+        targetHeading.style.color = "white";
     }
 }
 
-htmlIcon.addEventListener("click", function () {defineLanguage(htmlBox)});
-cssIcon.addEventListener("click", function () {defineLanguage(cssBox)});
-javascriptIcon.addEventListener("click", function () {defineLanguage(javascriptBox)});
-gitIcon.addEventListener("click", function () {defineLanguage(gitBox)});
-githubIcon.addEventListener("click", function () {defineLanguage(githubBox)});
-nodeIcon.addEventListener("click", function () {defineLanguage(nodeBox)});
+htmlIcon.addEventListener("click", function () {showLanguageDescription(htmlHeading, htmlDescription)});
+cssIcon.addEventListener("click", function () {showLanguageDescription(cssHeading, cssDescription)});
+javascriptIcon.addEventListener("click", function () {showLanguageDescription(javascriptHeading, javascriptDescription)});
+gitIcon.addEventListener("click", function () {showLanguageDescription(gitHeading, gitDescription)});
+githubIcon.addEventListener("click", function () {showLanguageDescription(githubHeading, githubDescription)});
+nodeIcon.addEventListener("click", function () {showLanguageDescription(nodeHeading, nodeDescription)});
